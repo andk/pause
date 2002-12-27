ddb13ebb4e81fdb12758e1a066b3ff9a428b6eef
k <k@a0804923-bdb8-0310-8f3c-e6a281b66435>
2002-12-27 22:25:55 +0000

    svndump.pl bugfixing
    git-svn-id: https://pause.perl.org:5460/svn/pause/trunk@53 a0804923-bdb8-0310-8f3c-e6a281b66435

--- cron/svn-dump.pl
+++ cron/svn-dump.pl
@@ -3,8 +3,7 @@
 
 =pod
 
-Write an svn dump, check that same version out in a temporary
-directory, make a tarball of that.
+Check current version out in a temporary directory, make a tarball.
 
 =cut
 
@@ -24,37 +23,19 @@ unless (-d $DIR) {
 my $tdir = File::Temp::tempdir("TEMP-XXXXXX", DIR => "/tmp", CLEANUP=>1);
 chdir $tdir or die "Could not chdir to $tdir: $!";
 
-my @m=gmtime;
-$m[5]+=1900;
-$m[4]++;
-my $D = sprintf "%04d%02d%02d%02d%02dGMT",@m[5,4,3,2,1];
-
 my($repopath) = $PAUSE::Config->{SVNPATH};
 
-my $dout = "svn.dump";
-my $derr = "svn.err";
-
-my $system = "$PAUSE::Config->{SVNBIN}/svnadmin dump $repopath > $dout 2> $derr";
-system($system)==0 or die "Could not svnadmin";
-
-open my $fh, $derr or die "Could not open $derr";
+$system = "$PAUSE::Config->{SVNBIN}/svn co file://$repopath pause-wc |";
 my $revision;
-while (<$fh>) {
-  $revision = $1 if /Dumped revision (\d+)/;
+open my $job, $system or die;
+while (<$job>) {
+  $revision = $1 if /Checked out revision (\d+)/;
 }
-
-$system = "$PAUSE::Config->{SVNBIN}/svn co -r $revision file://$repopath pause-wc-$revision";
-system($system)==0 or die "Could not svn co";
+die "No revision?" unless $revision;
+rename "pause-wc", "pause-wc-$revision" or die "Could not rename: $!";
 
 $system = "tar cjf pause-wc-$revision.tar.bz2 pause-wc-$revision";
 system($system)==0 or die "Could not svn co";
-
-$system = "bzip2 -9 $dout";
-system($system)==0 or die "Could not bzip2";
-
-# warn "$dout.bz2 -> $DIR/pause-svndump-$revision.bz2";
-copy "$dout.bz2", "$DIR/pause-svndump-$revision.bz2" or die
-    unless -e "$DIR/pause-svndump-$revision.bz2";
 
 # warn "pause-wc-$revision.tar.bz2 -> $DIR/pause-wc-$revision.tar.bz2";
 copy "pause-wc-$revision.tar.bz2", "$DIR/pause-wc-$revision.tar.bz2" or die
