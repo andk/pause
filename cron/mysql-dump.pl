dc2f1a41c288e5bfdbd11dd9e86a32baad9ae8fe
k <k@a0804923-bdb8-0310-8f3c-e6a281b66435>
2002-12-27 16:13:48 +0000

    add mysql-dump.pl
    git-svn-id: https://pause.perl.org:5460/svn/pause/trunk@40 a0804923-bdb8-0310-8f3c-e6a281b66435

---
+++ cron/mysql-dump.pl
@@ -0,0 +1,32 @@
+#!/usr/bin/perl -w
+
+use strict;
+
+my $DIR = "/home/ftp/pub/PAUSE/PAUSE-data";
+my @m=gmtime;
+$m[5]+=1900;
+$m[4]++;
+my $D = sprintf "%04d%02d%02d%02d%02dGMT",@m[5,4,3,2,1];
+
+use lib "/home/k/PAUSE/lib";
+use PAUSE ();
+# unless ($Dbh = DBI->connect(
+#                             $PAUSE::Config->{MOD_DATA_SOURCE_NAME},
+#                             $PAUSE::Config->{MOD_DATA_SOURCE_USER},
+#                             $PAUSE::Config->{MOD_DATA_SOURCE_PW},
+#                             { RaiseError => 1 }
+#                            )) {
+#   die "Connect to database not possible: $DBI::errstr\n";
+# }
+
+
+system "/usr/local/bin/mysqldump --lock-tables --add-drop-table -u '$PAUSE::Config->{MOD_DATA_SOURCE_USER}' -P '$PAUSE::Config->{MOD_DATA_SOURCE_PW}' '$PAUSE::Config->{MOD_DATA_SOURCE_NAME}' > $DIR/.moddump.current";
+
+my $BZIP = "/usr/local/bin/bzip2";
+$BZIP = "/usr/bin/bzip2" unless -x $BZIP;
+die "where is BZIP" unless -x $BZIP;
+
+rename "$DIR/.moddump.current", "$DIR/moddump.current";
+unlink "$DIR/moddump.current.bz2";
+system "$BZIP -9 --keep --small $DIR/moddump.current";
+system "/bin/cp $DIR/moddump.current.bz2 $DIR/moddump.$D.bz2";
