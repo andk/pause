94ffb7ee507197b87f7d5b101ec292e6abc7420c
k <k@a0804923-bdb8-0310-8f3c-e6a281b66435>
2003-01-11 10:10:05 +0000

    cleanup-incoming.pl: bugfix
    git-svn-id: https://pause.perl.org:5460/svn/pause/trunk@103 a0804923-bdb8-0310-8f3c-e6a281b66435

--- cron/cleanup-incoming.pl
+++ cron/cleanup-incoming.pl
@@ -5,6 +5,7 @@ my $Id = q$Id$;
 use lib "/home/k/PAUSE/lib";
 use PAUSE ();
 use DBI;
+use File::Spec;
 
 my $incdir = $PAUSE::Config->{INCOMING_LOC};
 
@@ -20,10 +21,11 @@ my $sth = $dbh->prepare("SELECT * FROM uris where uri=?");
 opendir DIR, $incdir or die;
 for my $dirent (readdir DIR) {
   next if $dirent =~ /^\.(|\.|message)\z/;
-  my $absdirent = "$incdir/$dirent";
+  my $absdirent = File::Spec->catfile($incdir,$dirent);
   next unless -f $absdirent;
   next if -M $absdirent < 1/24;
   $sth->execute($dirent);
   next if $sth->rows > 0;
   warn "Should unlink $absdirent";
 }
+closedir DIR;
