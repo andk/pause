69fcd278b9bacb2500940192ed57ac6090e44064
k <k@a0804923-bdb8-0310-8f3c-e6a281b66435>
2003-01-11 10:01:33 +0000

    cleanup-incoming.pl: first cut
    git-svn-id: https://pause.perl.org:5460/svn/pause/trunk@100 a0804923-bdb8-0310-8f3c-e6a281b66435

--- cron/cleanup-incoming.pl
+++ cron/cleanup-incoming.pl
@@ -4,4 +4,25 @@ my $Id$;
 
 use lib "/home/k/PAUSE/lib";
 use PAUSE ();
+use DBI;
 
+my $incdir = $PAUSE::Config->{INCOMING_LOC};
+
+my $dbh = DBI->connect(
+                       $PAUSE::Config->{MOD_DATA_SOURCE_NAME},
+                       $PAUSE::Config->{MOD_DATA_SOURCE_USER},
+                       $PAUSE::Config->{MOD_DATA_SOURCE_PW},
+                       { RaiseError => 1 }
+                      );
+
+my $sth = $dbh->prepare("SELECT * FROM uris where uri=?");
+
+opendir DIR, $incdir or die;
+for my $dirent (readdir DIR) {
+  next if $dirent =~ /^\.(|\.|message)\z/;
+  my $absdirent = "$incdir/$dirent";
+  next if -M $absdirent < 1/24;
+  $sth->execute($dirent);
+  next if $sth->rows > 0;
+  warn "Should unlink $absdirent";
+}
