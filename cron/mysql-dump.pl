c4edfa859041e90e0df4f84ef1e6fd4ccfbb38f7
k <k@a0804923-bdb8-0310-8f3c-e6a281b66435>
2003-01-17 09:35:24 +0000

    cron/mysql-dump.pl: rewrite to allow for more databases to be dumped
    git-svn-id: https://pause.perl.org:5460/svn/pause/trunk@118 a0804923-bdb8-0310-8f3c-e6a281b66435

--- cron/mysql-dump.pl
+++ cron/mysql-dump.pl
@@ -2,24 +2,36 @@
 
 use strict;
 
+our $Id = q$Id$;
+
 use lib "/home/k/PAUSE/lib";
 use PAUSE ();
-my $DIR = "$PAUSE::Config->{FTPPUB}/PAUSE-data";
 my @m=gmtime;
 $m[5]+=1900;
 $m[4]++;
 my $D = sprintf "%04d%02d%02d%02d%02dGMT",@m[5,4,3,2,1];
-
-my($dbi,$dbengine,$db) = split /:/, $PAUSE::Config->{MOD_DATA_SOURCE_NAME};
-die "Script would not work for $dbengine" unless $dbengine =~ /mysql/i;
-
-system "/usr/local/bin/mysqldump --lock-tables --add-drop-table --user='$PAUSE::Config->{MOD_DATA_SOURCE_USER}' --password='$PAUSE::Config->{MOD_DATA_SOURCE_PW}' '$db' > $DIR/.moddump.current";
-
 my $BZIP = "/usr/local/bin/bzip2";
 $BZIP = "/usr/bin/bzip2" unless -x $BZIP;
 die "where is BZIP" unless -x $BZIP;
 
-rename "$DIR/.moddump.current", "$DIR/moddump.current";
-unlink "$DIR/moddump.current.bz2";
-system "$BZIP -9 --keep --small $DIR/moddump.current";
-system "/bin/cp $DIR/moddump.current.bz2 $DIR/moddump.$D.bz2";
+my $Struct = [
+              {backupdir => "$PAUSE::Config->{FTPPUB}/PAUSE-data",
+               cfg_dsn => "MOD_DATA_SOURCE_NAME",
+               cfg_user => "MOD_DATA_SOURCE_USER",
+               cfg_pw => "MOD_DATA_SOURCE_PW"},
+];
+for my $struct (@$Struct) {
+  my $backup_dir = $struct->{backupdir};
+  my($dbi,$dbengine,$db) = split /:/, $PAUSE::Config->{$struct->{cfg_dsn}};
+  die "Script would not work for $dbengine" unless $dbengine =~ /mysql/i;
+  my $user = $PAUSE::Config->{$struct->{cfg_user}};
+  my $password = $PAUSE::Config->{$struct->{cfg_pw}};
+  for my $var ($db,$user,$password) {
+    die "illegal variable value[$val]" if $var =~ /['";]/;
+  }
+  system "/usr/local/bin/mysqldump --lock-tables --add-drop-table --user='$user' --password='$password' '$db' > $backup_dir/.moddump.current";
+  rename "$backup_dir/.moddump.current", "$backup_dir/moddump.current";
+  unlink "$backup_dir/moddump.current.bz2";
+  system "$BZIP -9 --keep --small $backup_dir/moddump.current";
+  system "/bin/cp $backup_dir/moddump.current.bz2 $backup_dir/moddump.$D.bz2";
+}
