9f05cd46630fb67c6bc3318bf51639ed21283ab5
k <k@a0804923-bdb8-0310-8f3c-e6a281b66435>
2003-02-18 15:41:55 +0000

    funet-watcher: not strict clean
    git-svn-id: https://pause.perl.org:5460/svn/pause/trunk@186 a0804923-bdb8-0310-8f3c-e6a281b66435

--- cron/funet-broken.pl
+++ cron/funet-broken.pl
@@ -11,7 +11,7 @@ my $ftp = Net::FTP->new("ftp.funet.fi");
 $ftp->login("anonymous","k\@pause.perl.org");
 $ftp->cwd("/pub/languages/perl/CPAN/modules") or die;
 ($_) = grep /02packages.details.txt.gz/, $ftp->dir;
-@l = split;
+my @l = split;
 my $statusfile = "/var/run/netreport/funet_broken.status";
 if ("@l[5,6,7]" eq "Feb 10 14:55"){
   open my $fh, ">", $statusfile;
