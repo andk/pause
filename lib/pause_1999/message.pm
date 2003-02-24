c73c483b5679c1198e707aa107323781634ee78d
k <k@a0804923-bdb8-0310-8f3c-e6a281b66435>
2003-02-24 14:11:15 +0000

    * message.pm:
      -- playing around how it should look like
    
    
    
    
    git-svn-id: https://pause.perl.org:5460/svn/pause/trunk@238 a0804923-bdb8-0310-8f3c-e6a281b66435

--- lib/pause_1999/message.pm
+++ lib/pause_1999/message.pm
@@ -4,7 +4,7 @@ use base 'Class::Singleton';
 use pause_1999::main;
 use strict;
 use utf8;
-our $VERSION = sprintf "%d", q$Rev: 235 $ =~ /(\d+)/;
+our $VERSION = sprintf "%d", q$Rev$ =~ /(\d+)/;
 
 sub as_string {
   my pause_1999::message $self = shift;
@@ -16,12 +16,12 @@ sub as_string {
   my $sth = $dbh->prepare("select * from messages where mto=?");
   $sth->execute($user);
   if ($sth->rows > 0) {
-    push @m, qq{<div align="left">};
+    push @m, qq{<div class="messages">};
     push @m, qq{<p>Messages to user $user:</p>};
     push @m, qq{<dl>};
     while (my $rec = $sth->fetchrow_hashref) {
-      push @m, qq{<dt>$rec->{created} by $rec->{mfrom}</dt>};
-      push @m, qq{<dd>$rec->{what}</dd>};
+      push @m, qq{<dt>$rec->{created} from $rec->{mfrom}</dt>};
+      push @m, qq{<dd>$rec->{message}</dd>};
     }
     push @m, qq{</dl>};
     push @m, qq{<p>Please answer the sender so that they can delete the messages.</p>};
