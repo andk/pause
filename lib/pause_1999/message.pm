c85010dfff5860423041c281ba133c2da90cff19
k <k@a0804923-bdb8-0310-8f3c-e6a281b66435>
2003-03-04 05:24:53 +0000

    message.pm: formatting of the message board
    git-svn-id: https://pause.perl.org:5460/svn/pause/trunk@265 a0804923-bdb8-0310-8f3c-e6a281b66435

--- lib/pause_1999/message.pm
+++ lib/pause_1999/message.pm
@@ -17,16 +17,19 @@ sub as_string {
   $sth->execute($user);
   if ($sth->rows > 0) {
     push @m, qq{<div class="messages">};
-    push @m, qq{<p>The following personal messages to user $user are stored:</p>};
+    push @m, qq{<p><b>Message board</b> for user <b>$user</b>:</p>};
     push @m, qq{<dl>};
     while (my $rec = $sth->fetchrow_hashref) {
-      push @m, qq{<dt>$rec->{created} from $rec->{mfrom}\@cpan.org</dt>};
+      push @m, qq{<dt><b>$rec->{created}</b> from $rec->{mfrom}\@cpan.org</dt>};
       push @m, qq{<dd>};
       push @m, $mgr->escapeHTML($rec->{message});
       push @m, qq{</dd>};
     }
     push @m, qq{</dl>};
-    push @m, qq{<p>Please answer the sender so that they can delete the messages.</p>};
+
+    push @m, qq{<p><b>Note:</b> Only the poster of a message can
+        delete it. Please contact them.</p>};
+
     push @m, qq{</div>\n};
   }
   @m;
