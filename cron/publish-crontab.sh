c7dbe187745e44f2acfc0c0ede4ff273543edabc
k <k@a0804923-bdb8-0310-8f3c-e6a281b66435>
2003-07-26 06:59:23 +0000

    Revision 387 was an error
    git-svn-id: https://pause.perl.org:5460/svn/pause/trunk@388 a0804923-bdb8-0310-8f3c-e6a281b66435

--- cron/publish-crontab.sh
+++ cron/publish-crontab.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 
-if ! cmp /var/spool/cron/root /home/k/PAUSE/cron/crontab.root ; then
-    diff -u /home/k/PAUSE/cron/crontab.root /var/spool/cron/root
+if [ ! -d /home/k/PAUSE/tmp ] ; then
+  mkdir /home/k/PAUSE/tmp
+fi
+CRONTMP=/home/k/PAUSE/tmp/crontab.root
+CRONREPO=/home/k/PAUSE/cron/CRONTAB.ROOT
+crontab -u root -l > $CRONTMP
+if ! cmp $CRONTMP $CRONREPO ; then
+    diff -u $CRONREPO $CRONTMP
     echo
     echo END OF DIFF
     set -x
-    cp /var/spool/cron/root /home/k/PAUSE/cron/crontab.root
-    chmod 644 /home/k/PAUSE/cron/crontab.root
+    cp $CRONTMP $CRONREPO
+    chmod 644 $CRONREPO
 fi
