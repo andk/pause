c9fe9203ea0dcc6d1740bb0ff4572af76722a4f2
k <k@a0804923-bdb8-0310-8f3c-e6a281b66435>
2003-07-27 10:33:42 +0000

    call to mirror had a space too much
    git-svn-id: https://pause.perl.org:5460/svn/pause/trunk@398 a0804923-bdb8-0310-8f3c-e6a281b66435

--- cron/run_mirrors.sh
+++ cron/run_mirrors.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-perl /usr/bin/mirror -C /home/k/PAUSE/mirror/mirror.defaults /home/k/PAUSE/mirror/mymirror.config
+perl /usr/bin/mirror -C/home/k/PAUSE/mirror/mirror.defaults /home/k/PAUSE/mirror/mymirror.config
