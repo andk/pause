63769c544c13ce4d619a3ce0343833bc6dbc6353
k <k@a0804923-bdb8-0310-8f3c-e6a281b66435>
2003-08-25 12:12:48 +0000

    New limits for mon on the partition sizes
    git-svn-id: https://pause.perl.org:5460/svn/pause/trunk@430 a0804923-bdb8-0310-8f3c-e6a281b66435

--- cron/publish-crontab.sh
+++ cron/publish-crontab.sh
@@ -3,9 +3,12 @@
 if [ ! -d /home/k/PAUSE/tmp ] ; then
   mkdir /home/k/PAUSE/tmp
 fi
+
 CRONTMP=/home/k/PAUSE/tmp/crontab.root
 CRONREPO=/home/k/PAUSE/cron/CRONTAB.ROOT
+
 crontab -u root -l > $CRONTMP
+
 if ! cmp $CRONTMP $CRONREPO ; then
     diff -u $CRONREPO $CRONTMP
     echo
