99fe6784b2c4db8019c1c4ee9003f3448daae280
k <k@a0804923-bdb8-0310-8f3c-e6a281b66435>
2005-11-09 05:56:21 +0000

    add Getopt::Long and debugging aids to test ourselves
    git-svn-id: https://pause.perl.org:5460/svn/pause/trunk@654 a0804923-bdb8-0310-8f3c-e6a281b66435

--- cron/update-checksums.pl
+++ cron/update-checksums.pl
@@ -1,18 +1,25 @@
 #!/usr/local/bin/perl -w
 
 use CPAN::Checksums 1.018;
+use File::Copy qw(cp);
 use File::Find;
 use strict;
-use vars qw($DEBUG);
 
 use lib "/home/k/PAUSE/lib";
 use PAUSE ();
 
+use Getopt::Long;
+our %Opt;
+GetOptions(\%Opt,
+           "max=i",
+           "debug!",
+          ) or die;
+$Opt{debug} ||= 0;
 my $root = $PAUSE::Config->{MLROOT};
 
-# $max: 15 was really slow, 100 is fine, 1000 was necessary recently
+# max: 15 was really slow, 100 is fine, 1000 was necessary recently
 # because of key-expiration on 2005-02-02
-my $max = 100;
+$Opt{max} ||= 100;
 
 my $cnt = 0;
 
@@ -25,7 +32,7 @@ $CPAN::Checksums::MIN_MTIME_CHECKSUMS =
     $PAUSE::Config->{MIN_MTIME_CHECKSUMS} || 0;
 
 find(sub {
-       return if $cnt>=$max;
+       return if $cnt>=$Opt{max};
        return unless $File::Find::name =~ m|id/.|;
        return if -l;
        return unless -d;
@@ -37,9 +44,10 @@ find(sub {
        # which reason this will be, but it may happen. Something like
        # $CPAN::Checksums::FORCE_UPDATE?
 
+       cp "$File::Find::name/CHECKSUMS", "$File::Find::name/CHECKSUMS.bak" or die $! if $Opt{debug};
        my $ret = CPAN::Checksums::updatedir($File::Find::name);
        return if $ret == 1;
-       warn "name[$File::Find::name]\n" if $DEBUG;
+       warn "name[$File::Find::name]\n" if $Opt{debug};
        $cnt++;
      }, $root);
 
