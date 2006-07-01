b3afc7db87538eb5719fc85c82ae584f8140f334
k <k@a0804923-bdb8-0310-8f3c-e6a281b66435>
2006-07-01 07:45:22 +0000

    gives a good overview now
    git-svn-id: https://pause.perl.org:5460/svn/pause/trunk@806 a0804923-bdb8-0310-8f3c-e6a281b66435

--- cron/csync2-logfile-summary.pl
+++ cron/csync2-logfile-summary.pl
@@ -2,19 +2,16 @@
 
 use strict;
 use warnings;
-use Text::Wrap;
-$Text::Wrap::huge = "overflow";
-$Text::Wrap::columns = $ENV{COLUMNS}||80;
 
 my %jobs;
 while (<>) {
   chomp;
   my($date,$time,$proc,$what) = split " ", $_, 4;
   if ($what =~ /reached/) { # later ^EOJ
-    print wrap("","    ",sprintf "%5d: %s|%s %s %s\n", $proc, $jobs{$proc}, $date, $time, $what);
+    printf "%5d: %s\n_______%s %s %s\n", $proc, $jobs{$proc}, $date, $time, $what;
     delete $jobs{$proc};
   } elsif ($jobs{$proc}) {
-    $jobs{$proc} .= "|$date $time $what";
+    $jobs{$proc} .= "\n       $date $time $what";
   } else {
     $jobs{$proc} = "$date $time $what";
   }
