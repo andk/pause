c278bb64ceec8d9ad23036b827d83ae943a8ce1b
k <k@a0804923-bdb8-0310-8f3c-e6a281b66435>
2007-12-10 20:12:26 +0000

    new downtime NOW
    git-svn-id: https://pause.perl.org:5460/svn/pause/trunk@955 a0804923-bdb8-0310-8f3c-e6a281b66435

--- lib/PAUSE.pm
+++ lib/PAUSE.pm
@@ -136,8 +136,8 @@ downtime, then we're back to normal operation.
 
 sub downtimeinfo {
   return +{
-           downtime => 1197133200,
-           willlast => 5400,
+           downtime => 1197317508,
+           willlast => 1800,
           };
 }
 
