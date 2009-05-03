164062e97c1b9910c9c2617f3a46ea7711e56e65
Andreas J. Koenig <k@k5.linux.bogus>
2009-05-03 20:47:20 +0200

    oops

--- cron/sync-04pause.pl
+++ cron/sync-04pause.pl
@@ -4,11 +4,12 @@ use lib "/home/k/pause/lib";
 use PAUSE ();
 
 $ENV{LANG} = "C";
-open my $fh, "-|", "rsync -av /home/k/pause/htdocs/0*.html /home/ftp/pub/PAUSE/modules/" or die "Could not fork: $!";
+my $target = "/home/ftp/pub/PAUSE/modules/";
+open my $fh, "-|", "rsync -av /home/k/pause/htdocs/0*.html $target" or die "Could not fork: $!";
 while (<$fh>) {
     next if /^building file list/;
     last if /^wrote \d+/;
     last if /^total size is/;
     chomp;
-    PAUSE::newfile_hook($_);
+    PAUSE::newfile_hook("$target/$_");
 }
