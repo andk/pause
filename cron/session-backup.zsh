61ea9d7a004524ab8a2c699f7d76aeb28b143ead
Andreas J. Koenig <andk@cpan.org>
2010-02-27 08:30:37 +0100

    no more fiddle about in /var/run

--- cron/session-backup.zsh
+++ cron/session-backup.zsh
@@ -1,7 +1,7 @@
 #!/usr/bin/zsh
 
 set -e
-cd /var/run/PAUSE-httpd/rundata/pause_1999/
+cd /usr/local/apache/rundata/pause_1999/
 ls -laR session > session-repo/ls-laR.txt
 rsync -va session/ session-repo/ > session-repo/rsync.log
 cd session-repo
