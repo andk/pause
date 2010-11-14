472d313a6331690b0000328977f03703df5b8144
Andreas J. Koenig <andk@cpan.org>
2010-11-14 17:31:25 +0100

    sort by type (user/mod)

--- lib/pause_1999/edit.pm
+++ lib/pause_1999/edit.pm
@@ -301,12 +301,15 @@ sub manage_id_requests { # was reject_id_request
             unlink $path or die "Could not unlink '$path': $!";
             return;
           }
+          my $type;
           if (exists $session->{APPLY}{fullname}) {
             $sthu->execute($userid);
             return if $sthu->rows > 0;
+            $type = "user";
           } elsif (exists $session->{APPLY}{modid}) {
             $sthm->execute($session->{APPLY}{modid});
             return if $sthm->rows > 0;
+            $type = "mod";
           }
           if ($session->{APPLY}{rationale} =~ /\b(?:BLONDE\s+NAKED|NAKED\s+SEXY|FREE\s+CUMSHOT|CUMSHOT\s+VIDEOS|FREE\s+SEX|FREE\s+TUBE|GROUP\s+SEX|FREE\s+PORN|SEX\s+VIDEO|SEX\s+MOVIES?|SEX\s+TUBE|SEX\s+MATURE|STREET\s+BLOWJOBS|SEX\s+PUBLIC|TUBE\s+PORN|PORN\s+TUBE|TUBE\s+VIDEOS|VIDEO\s+TUBE|XNXX\s+VIDEOS|XXX\s+FREE|ANIMAL\s+SEX|GIRLS\s+SEX|PORN\s+VIDEOS?|PORN\s+MOVIES|TITS\s+PORN|RAW\s+SEX|DEEPTHROAT\s+TUBE|celeb\s+porn|PREGNANT\s+TUBE|picture\s+sex|NAKED\s+WOMEN|WOMEN\s+MOVIES|MATURE\s+NAKED|SEX\s+ANIME|hot\s+nude|nude\s+celebs|ANIME\s+TUBES|SEX\s+DOG|MATURE\s+SEX|MATURE\s+PUSSY|Rape\s+Porn|brutal\s+fuck|rape\s+video|ANIMAL\s+TUBE|SHEMALE\s+CUMSHOT|ANIMAL\s+PORN|ANIMAP\s+CLIP|CLIP\s+SEX|PUBLIC\s+BLOWJOB|free\s+lesbian|lesbian\s+sex|SEX\s+ZOO|tv-adult|numismata.org|www.soulcommune.com|www.petsusa.org|www.csucssa.org|www.thisis50.com|www.comunidad-latina.net|www.singlefathernetwork.com|www.freetoadvertise.biz|gayforum.dk|www.purevolume.com|playgroup.themouthpiece.com|www.bananacorp.cl|party.thebamboozle.com|blog.tellurideskiresort.com|www.pethealthforums.com|www.burropride.com|lpokemon.19.forumer.com|Zootube365|Eskimotube|xtube-1|phentermine without a prescription)\b/i) {
             unlink $path or die "Could not unlink '$path': $!";
@@ -315,26 +318,30 @@ sub manage_id_requests { # was reject_id_request
           $ALL{$path} = {
                          session => $session,
                          mtime   => $mtime,
+                         type    => $type,
                         };
         },
         no_chdir => 1,
        },
        $mgr->{SessionDataDir}, # a bit under $PAUSE::Config->{RUNDATA}
       );
-push @m, qq{<p>view all pending applications for new user IDs and for modules registrations</p>
+push @m, qq{<p>View all pending applications for new user IDs and for modules registrations</p>
 <p><table class="administration">
-<tr><th>Userid</th><th>raw</th></tr>
+<tr><th>Type</th><th>Userid/Time</th><th>Raw Session</th></tr>
 };
   require YAML::Syck;
   require JSON::XS;
   my $jx = JSON::XS->new->indent->canonical;
-  for my $k (sort { $ALL{$b}{mtime} <=> $ALL{$a}{mtime} } keys %ALL) {
+  for my $k (sort { $ALL{$b}{type} cmp $ALL{$a}{type} || $ALL{$b}{mtime} <=> $ALL{$a}{mtime} } keys %ALL) {
     my $esc = $mgr->escapeHTML($jx->encode($ALL{$k}{session}));
     #$esc =~ s/ /&nbsp;/g;
     $esc =~ s/\\n/<br\/>/g;
     push @m, sprintf
         (
          '<tr><td class="administration" valign="top">
+%s
+</td>
+<td valign="top">
 %s
 <br/>
 %s
@@ -346,6 +353,7 @@ push @m, qq{<p>view all pending applications for new user IDs and for modules re
 </td><td valign="top">%s</td>
 <td valign="top"><div id="contentBox"><pre style="overflow: auto; position: relative; height: auto; vertical-align: top">%s</pre></div></td></tr>
 ',
+         $ALL{$k}{type},
          $ALL{$k}{session}{APPLY}{userid},
          POSIX::strftime("%FT%TZ", gmtime $ALL{$k}{mtime}),
          exists $ALL{$k}{session}{APPLY}{fullname} ? "add_user" : "add_mod",
