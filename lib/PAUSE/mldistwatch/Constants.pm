0d3be4a60c4ccc446c54987bd8a34bb1fc210be7
David Golden <dagolden@cpan.org>
2011-08-13 16:52:48 -0400

    Break out PAUSE::dist and PAUSE::mldistwatch::Constants

---
+++ lib/PAUSE/mldistwatch/Constants.pm
@@ -0,0 +1,18 @@
+use strict;
+use warnings;
+package PAUSE::mldistwatch::Constants;
+
+# constants used for index_status:
+use constant EDUALOLDER => 50; # pumpkings only
+use constant EDUALYOUNGER => 30; # pumpkings only
+use constant EOPENFILE => 21;
+use constant EMISSPERM => 20;
+use constant EPARSEVERSION => 10;
+use constant EOLDRELEASE => 4;
+use constant EMTIMEFALLING => 3; # deprecated after rev 478
+use constant EVERFALLING => 2;
+use constant OK => 1;
+
+1;
+
+
