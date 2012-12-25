9d508335a79dedde8b676e6b4b284d8c70191119
Andreas Koenig <andk@cpan.org>
2012-12-25 18:35:36 +0100

    gcc-g++ missing for Perlbal::XS::HTTPHeaders

--- etc/puppet/manifests/site.pp
+++ etc/puppet/manifests/site.pp
@@ -11,6 +11,7 @@ class pause-pkg {
 	package { mysql-devel  : ensure => installed }
 	package { unzip        : ensure => installed }
 	package { git          : ensure => installed }
+	package { gcc-g++      : ensure => installed }
 }
 
 class pause-mysqld {
