62dcc893a4bdd816d131e4da55d1f139ab7bb8ed
Andreas Koenig <andk@cpan.org>
2013-01-02 15:07:14 +0100

    remove unused code

--- cron/mirror_wget_rsync.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/zsh
-
-
-# wget --directory-prefix=/home/ftp/pub/PAUSE/authors/id/M/MU/MUIR --cut-dirs=3 --no-host-directories --verbose --append-output=/var/log/wget --accept gz -r -N -l 3  ftp://ftp.idiom.com/users/muir/CPAN
