74b1d2395adc42ec495bf2d680c64b7089400872
Andreas Koenig <andk@cpan.org>
2013-07-20 07:15:23 +0200

    reset to pre-May speed

--- cron/update-checksums.pl
+++ cron/update-checksums.pl
@@ -101,7 +101,7 @@ our $TESTDIR;
 # now that we know that the process is not faster when we write less
 # (2005-11-11); but lower than 1000 helps to smoothen out peaks; 512
 # make a lot of noise on the rsyncs (2013-05-06)
-$Opt{max} ||= 192;
+$Opt{max} ||= 128;
 $Opt{"sleep-per-dir"} ||= 0.5;
 
 my $cnt = 0;
