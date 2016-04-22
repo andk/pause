89dfd29e397d813bd17fd1e87dcd2ccfeb4361a1
Andreas Koenig <andk@cpan.org>
2016-04-22 13:24:46 +0200

    Reverting the iptables change, having enought to worry about at the moment

--- etc/puppet/manifests/site.pp
+++ etc/puppet/manifests/site.pp
@@ -423,20 +423,20 @@ class pause-limits {
 		source => "puppet:///files/etc/security/limits.conf-pause-us",
 	}
 }
-# class pause-iptables {
-# 	file { "/etc/sysconfig/iptables-config":
-# 		owner  => root,
-# 		group  => root,
-# 		mode   => 644,
-# 		source => "puppet:///files/etc/sysconfig/iptables-config-pause-us",
-# 	}
-# 	file { "/etc/sysconfig/iptables":
-# 		owner  => root,
-# 		group  => root,
-# 		mode   => 644,
-# 		source => "puppet:///files/etc/sysconfig/iptables-pause-us",
-# 	}
-# }
+class pause-iptables {
+	file { "/etc/sysconfig/iptables-config":
+		owner  => root,
+		group  => root,
+		mode   => 644,
+		source => "puppet:///files/etc/sysconfig/iptables-config-pause-us",
+	}
+	file { "/etc/sysconfig/iptables":
+		owner  => root,
+		group  => root,
+		mode   => 644,
+		source => "puppet:///files/etc/sysconfig/iptables-pause-us",
+	}
+}
 class pause-mon {
 	file { "/usr/lib64/mon/mon.d/freespace.monitor":
 		owner  => root,
