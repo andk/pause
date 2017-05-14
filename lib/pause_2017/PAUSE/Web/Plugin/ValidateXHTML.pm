883e6fdc20ca3c220a26cbe6d862709560fca7a1
Kenichi Ishigaki <ishigaki@cpan.org>
2017-05-14 21:27:25 +0900

    move TagHelpers hack to ValidateXHTML plugin, so that we can remove only one stuff when we finish HTML5 transition

--- lib/pause_2017/PAUSE/Web/Plugin/ValidateXHTML.pm
+++ lib/pause_2017/PAUSE/Web/Plugin/ValidateXHTML.pm
@@ -7,6 +7,12 @@ use String::Random;
 sub register {
   my ($self, $app, $conf) = @_;
   $app->hook(after_render => \&_validate);
+
+  # tweak default TagHelpers to spit xml, and Parameters recognize ";" for now
+  {
+    no warnings 'redefine';
+    *Mojolicious::Plugin::TagHelpers::_tag = \&_fix_tag;
+  }
 }
 
 sub _validate {
@@ -29,6 +35,21 @@ sub _validate {
     }
   }
   return 1;
+}
+
+sub _fix_tag {
+  my $tree = ['tag', shift, undef, undef];
+
+  # Content
+  if (ref $_[-1] eq 'CODE') { push @$tree, ['raw', pop->()] }
+  elsif (@_ % 2) { push @$tree, ['text', pop] }
+
+  # Attributes
+  my $attrs = $tree->[2] = {@_};
+  if (ref $attrs->{data} eq 'HASH' && (my $data = delete $attrs->{data})) {
+    @$attrs{map { y/_/-/; lc "data-$_" } keys %$data} = values %$data;
+  }
+  return Mojo::ByteStream->new(Mojo::DOM::HTML::_render($tree, 'xml')); # TWEAKED
 }
 
 1;
