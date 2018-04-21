b6546ba9659a3ef8674f051daf2910a8e388dba6
David Golden <xdg@xdg.me>
2018-04-21 13:04:38 +0200

    Stub PAUSE::Permissions

---
+++ lib/PAUSE/Permissions.pm
@@ -0,0 +1,17 @@
+use 5.008001;
+use strict;
+use warnings;
+
+package PAUSE::Permissions;
+# ABSTRACT: goes here
+
+use Class::Tiny qw/dbh/;
+
+
+1;
+
+# Local Variables:
+# mode: cperl
+# cperl-indent-level: 2
+# End:
+# vim: set ts=2 sts=2 sw=2 et tw=75:
