f51e2e2f492ca9fc135305d62b5a434e468c6303
Kenichi Ishigaki <ishigaki@cpan.org>
2018-04-23 01:44:38 +0900

    Try to accept uploads via cpan-upload etc

--- lib/pause_2017/PAUSE/Web/Controller/Root.pm
+++ lib/pause_2017/PAUSE/Web/Controller/Root.pm
@@ -19,6 +19,20 @@ sub check {
 
 sub index {
   my $c = shift;
+  my $pause = $c->stash(".pause");
+  my $mgr = $c->app->pause;
+  my $req = $c->req;
+  my $u = $c->active_user_record;
+
+  # Special case for cpan-uploaders that post to the /pause/authenquery without any ACTION
+  return unless $u->{userid};
+  return unless uc $req->method eq 'POST';
+  return unless $req->param('SUBMIT_pause99_add_uri_HTTPUPLOAD' || $req->param('SUBMIT_pause99_add_uri_httpupload');
+
+  my $action = 'add_uri';
+  $req->param('ACTION' => $action);
+  $pause->{Action} = $action;
+  return $c->delegate($action);
 }
 
 sub auth {
