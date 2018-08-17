75e4c432d3508faddde994abbd80cca51b3bdb55
Kenichi Ishigaki <ishigaki@cpan.org>
2018-08-17 21:44:16 +0900

    Wrap dispatch instead of action to catch more errors correctly

--- lib/pause_2017/PAUSE/Web/Exception.pm
+++ lib/pause_2017/PAUSE/Web/Exception.pm
@@ -2,7 +2,7 @@ package PAUSE::Web::Exception;
 
 use Mojo::Base -base;
 use overload 
-  '""' => sub {$_[0]->{ERROR}},
+  '""' => sub {$_[0]->{ERROR} ? $_[0]->{ERROR} : $_[0]->{HTTP_STATUS} ? $_[0]->{HTTP_STATUS} : ""},
 ;
 
 
--- lib/pause_2017/PAUSE/Web/Plugin/WrapAction.pm
+++ lib/pause_2017/PAUSE/Web/Plugin/WrapAction.pm
@@ -1,18 +1,22 @@
 package PAUSE::Web::Plugin::WrapAction;
 
 use Mojo::Base "Mojolicious::Plugin";
-use HTTP::Status qw/:constants/;
+use HTTP::Status qw/:constants status_message/;
 
 sub register {
   my ($self, $app, $conf) = @_;
 
-  $app->hook(around_action => \&_wrap);
+  $app->hook(around_dispatch => \&_wrap);
 }
 
 sub _wrap {
   my ($next, $c, $action, $last) = @_;
 
   my $pause = $c->stash(".pause");
+  if (!$pause) {
+    $pause = {};
+    $c->stash(".pause", $pause);
+  }
 
   my $res = eval { $next->(); };
   if ($@) {
@@ -22,8 +26,12 @@ sub _wrap {
         push @{$pause->{ERROR}}, @{$@->{ERROR}};
         require Data::Dumper;
         print STDERR "Line " . __LINE__ . ", File: " . __FILE__ . "\n" . Data::Dumper->new([$pause->{ERROR}],[qw(error)])->Indent(1)->Useqq(1)->Dump; # XXX
+        $c->res->code($@->{HTTP_STATUS}) if $@->{HTTP_STATUS};
+        $c->render('layouts/layout') unless $c->stash('Action');
       } elsif ($@->{HTTP_STATUS}) {
-        $c->res->code($@->{HTTP_STATUS});
+        $c->res->headers->content_type('text/plain');
+        $c->res->body(status_message($@->{HTTP_STATUS}));
+        $c->rendered($@->{HTTP_STATUS});
         return;
       }
     } else {
