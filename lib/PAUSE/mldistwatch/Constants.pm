36e369f7a4392e9023814ea125b25dac9cb2a426
Ricardo Signes <rjbs@semiotic.systems>
2023-04-29 21:26:38 +0200

    indexer constants: remove a bunch of dead constants

--- lib/PAUSE/mldistwatch/Constants.pm
+++ lib/PAUSE/mldistwatch/Constants.pm
@@ -5,7 +5,6 @@ package PAUSE::mldistwatch::Constants;
 # constants used for index_status:
 use constant EDUALOLDER => 50; # pumpkings only
 use constant EDUALYOUNGER => 30; # pumpkings only
-use constant EDISTNAMEPERM => 26;
 use constant EDBERR => 25;
 use constant EDBCONFLICT => 23;
 use constant EOPENFILE => 21;
@@ -13,34 +12,23 @@ use constant EMISSPERM => 20;
 use constant ELONGVERSION => 13;
 use constant EBADVERSION => 12;
 use constant EPARSEVERSION => 10;
-use constant ENOMETAFILE => 8;
-use constant E_DB_XACTFAIL => 7;
-use constant EMETAUNSTABLE => 6;
-use constant EBAREPMFILE => 5;
 use constant EOLDRELEASE => 4;
-use constant EMTIMEFALLING => 3; # deprecated after rev 478
 use constant EVERFALLING => 2;
 use constant OK => 1;
 
 our $heading = {
-  EBADVERSION() => "Version string is not a valid 'lax version' string",
-  ELONGVERSION() => "Version string exceeds maximum allowed length of 16b",
-  E_DB_XACTFAIL() => "ERROR: Database error occurred during index update",
-  EBAREPMFILE() => "Bare .pm files are not indexed",
-  EDBCONFLICT() => "Conflicting record found in index",
-  EDBERR() => "Database error",
-  EDUALOLDER() => "An older dual-life module stays reference",
-  EDUALYOUNGER() => "Dual-life module stays reference",
-  EDISTNAMEPERM() => "No permissions for distribution name",
-  EMISSPERM() => "Permission missing",
-  EMTIMEFALLING() => "Decreasing mtime on a file (category to be deprecated)",
-  ENOMETAFILE() => "Distribution included neither META.json nor META.yml",
-  EOLDRELEASE() => "Release seems outdated",
-  EOPENFILE() => "Problem while reading the distribtion",
-  EMETAUNSTABLE() => "META release_status is not stable, will not index",
+  EBADVERSION()   => "Version string is not a valid 'lax version' string",
+  ELONGVERSION()  => "Version string exceeds maximum allowed length of 16b",
+  EDBCONFLICT()   => "Conflicting record found in index",
+  EDBERR()        => "Database error",
+  EDUALOLDER()    => "An older dual-life module stays reference",
+  EDUALYOUNGER()  => "Dual-life module stays reference",
+  EMISSPERM()     => "Permission missing",
+  EOLDRELEASE()   => "Release seems outdated",
+  EOPENFILE()     => "Problem while reading the distribtion",
   EPARSEVERSION() => "Version parsing problem",
-  EVERFALLING() => "Decreasing version number",
-  OK() => "Successfully indexed",
+  EVERFALLING()   => "Decreasing version number",
+  OK()            => "Successfully indexed",
 };
 
 sub heading ($) {
@@ -50,5 +38,3 @@ sub heading ($) {
 }
 
 1;
-
-
