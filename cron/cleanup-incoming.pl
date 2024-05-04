d31b0070da6d32540c1dbaada450f29e60109c17
Ricardo Signes <rjbs@semiotic.systems>
2024-05-04 10:59:39 -0400

    cron/cleanup-incoming.pl: remove 20-years unreachable code

--- cron/cleanup-incoming.pl
+++ cron/cleanup-incoming.pl
@@ -36,14 +36,7 @@ for my $dirent (readdir DIR) {
       next if $sth->rows > 0;
   }
   my $size = -s $absdirent;
-  if (0 && $dirent =~ /^(\d+)\.(\d+)$/) { # these come often, but I could not decipher
-    open my $fh, $absdirent or die "Could not open $absdirent: $!";
-    local $/;
-    my $str = <$fh>;
-    substr($str,100*1024) = "" if length($str)> 100*1024;
-    require Data::Dumper;
-    warn sprintf "content[%s]\n", Data::Dumper::Dumper($str);
-  }
+
   unlink $absdirent or die "Could not unlink $absdirent: $!";
   warn "unlinked $absdirent ($size)\n";
 }
