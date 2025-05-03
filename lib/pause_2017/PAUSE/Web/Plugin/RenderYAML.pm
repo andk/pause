5bafae0f6757e0152a970afa8618e926cb10896a
Kenichi Ishigaki <ishigaki@cpan.org>
2025-05-03 16:42:31 +0900

    Set content_disposition and content_type headers before serving a generated YAML file

--- lib/pause_2017/PAUSE/Web/Plugin/RenderYAML.pm
+++ lib/pause_2017/PAUSE/Web/Plugin/RenderYAML.pm
@@ -12,6 +12,9 @@ sub register {
     local $YAML::Syck::ImplicitUnicode = 1;
     my $dump = YAML::Syck::Dump($data);
     my $edump = Encode::encode_utf8($dump);
+    my $action = $c->req->param('ACTION') || 'pause';
+    $c->res->headers->content_disposition("attachment; filename=$action.yaml");
+    $c->res->headers->content_type('application/yaml');
     $c->stash(format => "text");
     $c->render(text => $edump);
     return;
