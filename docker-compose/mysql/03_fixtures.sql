b21c041ce914959720995744c3ca72cf8f85c6a5
Thomas Klausner <domm@plix.at>
2026-04-24 15:33:57 +0200

    working local docker compose setup

---
+++ docker-compose/mysql/03_fixtures.sql
@@ -0,0 +1,6 @@
+INSERT INTO `grouptable` VALUES ('TESTADMIN','admin');
+
+INSERT INTO `users` VALUES ('TESTADMIN','unused','1970-01-01 00:00:00','testadmin@localhost','secr','','TESTADMIN Name',NULL,'',NULL,NULL,''),('TESTCNSRD','unused','1970-01-01 00:00:00','CENSORED','secr','','TESTCNSRD Name',NULL,'',NULL,NULL,''),('TESTUSER','unused','1970-01-01 00:00:00','testuser@localhost','secr','','TESTUSER Name',NULL,'',NULL,NULL,'');
+
+INSERT INTO `usertable` VALUES ('TESTADMIN','$2a$12$CoQEZPZVEzqlkQWPzzQTses.eK86BVM07tjtWCywrPGnbLsc/0eTC','testadmin@localhost',0,NULL,NULL,NULL),('TESTCNSRD','$2a$12$zV/qZSD4Oa6uB4onl0b/6uA0yrcELZWebBCZOeX0T9rlmD97qXWj6','testcnsrd@localhost',0,NULL,NULL,NULL),('TESTUSER','$2a$12$Xq5nX/5AI4y/lbPC/wHbIeTpGXY76u8F8go90i03umhwRpG0bjtG2','testuser@localhost',0,NULL,NULL,NULL);
+
