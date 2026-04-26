de0d2f937cb8328f6f3a6226cbc3dad01854b6fe
Kenichi Ishigaki <ishigaki@cpan.org>
2026-04-26 20:17:32 +0900

    Use longer token_id,  mark some columns not null, and add a unique index

--- one-off-utils/schemachange-2026-04.sql
+++ one-off-utils/schemachange-2026-04.sql
@@ -1,14 +1,15 @@
 CREATE TABLE auth_tokens (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
-  user VARCHAR(9),
-  token_id VARCHAR(8),
-  token_hash VARCHAR(64),
+  user VARCHAR(9) NOT NULL,
+  token_id VARCHAR(32) NOT NULL,
+  token_hash VARCHAR(64) NOT NULL,
   description VARCHAR(255),
   expires_at DATETIME NOT NULL,
   ip_ranges TEXT,
   scope TEXT,
   revoked TINYINT DEFAULT 0,
   created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  UNIQUE(user, token_id),
   INDEX(user, token_id, token_hash),
   INDEX(user, revoked)
 );
